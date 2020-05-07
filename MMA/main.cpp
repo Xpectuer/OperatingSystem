e102652a2b5e39c18110ea776cb45d81269d2bd9
Alex <zhengjiaye@126.com>
2020-05-07 11:52:28 +0800

    Initial Commit

---
+++ MMA/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MMA
+//
+//  Created by Alex on 07/05/2020.
+//  Copyright © 2020 郑嘉业. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
